9b19fb4cb64d33d69ce65cc2b72162a6060cd919
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-21 00:19:50 +0000

    Add component: Newsletter1734740382

--- src/ProjectComponents/index.ts
+++ src/ProjectComponents/index.ts
@@ -1,3 +1,4 @@
-export * as Footer1733935279 from './Footer1733935279'
 export * as Hero1733935279 from './Hero1733935279'
 export * as Header1733935279 from './Header1733935279'
+export * as Newsletter1734740382 from './Newsletter1734740382'
+export * as Footer1733935279 from './Footer1733935279'
